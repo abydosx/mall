3e1e3ace1b2a2f5e335b4fca8abd3e0a28676fe4
ishidaxie <ishidaxie@outlook.com>
2019-11-02 17:56:14 +0800

    2019/10/24

---
+++ src/main/Java/com/mall/controllor/backend/UserManageController.java
@@ -0,0 +1,38 @@
+package com.mall.controllor.backend;
+
+import com.mall.common.Const;
+import com.mall.common.ServerResponse;
+import com.mall.pojo.User;
+import com.mall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/manage/user")
+public class UserManageController {
+
+    @Autowired
+    private IUserService iUserService;
+
+    @RequestMapping(value="login.do",method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+            User user = response.getData();
+            if(user.getRole() == Const.Role.ROLE_ADMIN){
+                session.setAttribute(Const.CURRENT_USER,user);
+                return response;
+            }else{
+                return ServerResponse.CreateFailMessage("没有管理员权限");
+            }
+        }
+        return response;
+    }
+
+}
